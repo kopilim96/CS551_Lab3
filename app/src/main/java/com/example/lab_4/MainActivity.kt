24d814356b9fd17a1a315d25a951b376a1e5a3c8
Kopi Bryant <kopibryant55@gmail.com>
2024-02-12 00:47:01 +0000

    Completely Done

--- app/src/main/java/com/example/lab_4/MainActivity.kt
+++ app/src/main/java/com/example/lab_4/MainActivity.kt
@@ -1,26 +1,24 @@
 package com.example.lab_4
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFrom
-import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -33,14 +31,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab_4.ui.theme.Lab_4Theme
@@ -74,115 +69,134 @@ fun ListViewLayout(mealList: List<Meal>){
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MainLayout(){
+fun MainLayout() {
     val focusManager = LocalFocusManager.current
 
     var ascendingOrder by remember {
         mutableStateOf(true)
     }
-    var orderName by remember { mutableStateOf(false ) }
+    var orderName by remember { mutableStateOf(false) }
 
-    val itemList = remember{ mutableStateListOf <Meal> () }
-    val sortedItemList = remember(itemList, ascendingOrder) {
-        val sortedList = itemList.sortedBy { it.mealName }
-        if (!ascendingOrder) sortedList.reversed() else sortedList
-    }
+    val itemList = remember { mutableStateListOf<Meal>() }
+    itemList.addAll(MealList) // show default value
 
-    var mealName by remember { mutableStateOf("Meal" ) }
-    var calory by remember { mutableStateOf( "0" ) }
+    var mealName by remember { mutableStateOf("Meal") }
+    var calory by remember { mutableStateOf("0") }
 
-    
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Gray)
-    ){
-
-        ListViewLayout(MealList)
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Column(
-            modifier = Modifier
-                .background(Color.White)
-                .fillMaxWidth()
-                .padding(top = 35.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ){
-            // This is Meal
-            TextField(
-                modifier = Modifier
-                    .padding(all = 4.dp)
-                    .fillMaxWidth(0.7f),
-                value = mealName,
-                onValueChange = {
-                    mealName = it
-                },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Text,
-                    imeAction = ImeAction.Next
-                ),
-            )
-
-            // This is Calory
-            TextField(
-                modifier = Modifier
-                    .padding(all = 4.dp)
-                    .fillMaxWidth(0.7f),
-                value = calory,
-                onValueChange = {
-                    calory = it
-                },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number,
-                    imeAction = ImeAction.Done
-                ),
-                keyboardActions = KeyboardActions(
-                    onDone = {
-                        focusManager.clearFocus()
-
-                        itemList.add(Meal(mealName, calory))
-
-                        // Reset the value
-                        mealName = "Meal"
-                        calory = "0"
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        content = {
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                LazyColumn(
+                    modifier = Modifier.fillMaxHeight(0.7f)
+                ) {
+                    items(itemList) { meal ->
+                        MealCard(meal.mealName, meal.calory)
                     }
-                ),
-                label = { Text("Label") }
-            )
+                }
 
-            // Switch button
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(0.7f)
-                    .padding(all = 8.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.End
-            ){
-                Text(
-                    text = stringResource(R.string.text_orderBy),
-                    modifier = Modifier,
-                    color = MaterialTheme.colorScheme.primary
-                )
-                Switch(
+                Box(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    checked = orderName,
-                    onCheckedChange = {
-                        orderName = it
-                        ascendingOrder = !ascendingOrder
-                    },
-                )
-            } // end of Row-switch
-        } // end of Column
+                        .fillMaxWidth()
+                        .align(Alignment.BottomCenter)
+                        .background(Color.Gray)
+                        .fillMaxHeight(0.3f),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth(0.7f),
+                        horizontalAlignment = Alignment.End
+                    ) {
+                        // This is Meal
+                        TextField(
+                            modifier = Modifier
+                                .padding(all = 4.dp)
+                                .fillMaxWidth(),
+                            value = mealName,
+                            onValueChange = {
+                                mealName = it
+                            },
+                            keyboardOptions = KeyboardOptions.Default.copy(
+                                keyboardType = KeyboardType.Text,
+                                imeAction = ImeAction.Next
+                            ),
+                        )
 
-    } // end of column
+                        // This is Calory
+                        TextField(
+                            modifier = Modifier
+                                .padding(all = 4.dp)
+                                .fillMaxWidth(),
+                            value = calory,
+                            onValueChange = {
+                                calory = it
+                            },
+                            keyboardOptions = KeyboardOptions.Default.copy(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Done
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onDone = {
+                                    focusManager.clearFocus()
 
-} // end of mainLayout
+                                    // Add the meal to the MealList
+                                    itemList.add(Meal(mealName, calory))
+
+                                    // Reset the value
+                                    mealName = "Meal"
+                                    calory = "0"
+                                }
+                            ),
+                            label = { Text("Label") }
+                        )
+
+                        // Switch button
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(all = 8.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                text = stringResource(R.string.text_orderBy),
+                                modifier = Modifier,
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                            Switch(
+                                modifier = Modifier,
+                                checked = orderName,
+                                onCheckedChange = {
+                                    orderName = it
+                                    ascendingOrder = !ascendingOrder
+
+                                    // Sort the list based on the orderName and ascendingOrder
+                                    itemList.sortBy {
+                                        if (orderName) it.mealName else it.calory
+                                    }
+                                    if (!ascendingOrder) itemList.reverse()
+                                },
+                            )
+                        } // end of Row-switch
+
+                    } // end of Column
+
+                } // end of Box - bottom part
+
+            } // end of Box - inner Box
+
+        } // end of content
+
+    ) // end of Scaffold
+
+} // end of function
+
+
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/lab_4/MealList.kt
+++ app/src/main/java/com/example/lab_4/MealList.kt
@@ -1,20 +1,13 @@
 package com.example.lab_4
 
-val MealList = listOf(
+var MealList = listOf(
     Meal("McDonald", "500"),
     Meal("Spring Roll", "200"),
-    Meal("McDonald", "500"),
-    Meal("Spring Roll", "200"),
-    Meal("McDonald", "500"),
-    Meal("Spring Roll", "200"),
-    Meal("McDonald", "500"),
-    Meal("Spring Roll", "200"),
-    Meal("McDonald", "500"),
-    Meal("Spring Roll", "200"),
-    Meal("McDonald", "500"),
-    Meal("Spring Roll", "200"),
-    Meal("McDonald", "500"),
-    Meal("Spring Roll", "200"),
-    Meal("McDonald", "500"),
-    Meal("Spring Roll", "200"),
+    Meal("Curry Mee", "200"),
+//    Meal("Wan Tan Mee", "200"),
+//    Meal("Beef Steak", "200"),
+//    Meal("Chips", "200"),
+//    Meal("Munchy Box", "200"),
+//    Meal("Samosa", "200"),
+//    Meal("Egg Fried Rice", "200"),
 )
